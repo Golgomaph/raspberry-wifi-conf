f725200f1dc25d78bf681664ee3833446bcfb1fe
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2021-01-27 13:15:46 +0100

    Update vartofile.js

--- app/public/vartofile.js
+++ app/public/vartofile.js
@@ -1,6 +1,10 @@
 function save_installform()
 {
-    var bluetooth=document.forms["installform"]["bluetooth"].value;
-    alert(bluetooth);
+    var bluetooth=document.forms["installform"]["bluetooth"].checked;
+    var upnp=document.forms["installform"]["upnp"].checked;
+    var airplay=document.forms["installform"]["airplay"].checked;
+    var snapcast=document.forms["installform"]["snapcast"].checked;
+    var spotify=document.forms["installform"]["spotify"].checked;
+    alert("Bluetooth: " + bluetooth + " | UPnP: " + upnp + " | Airplay: " + airplay + " | Spotify: " + spotify + " | Snapcast: " + snapcast);
     return true;
 }
